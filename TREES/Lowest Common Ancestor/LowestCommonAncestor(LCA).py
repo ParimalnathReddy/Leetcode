cc3f5aea8b21fd58be68e4e32c6976910beb8ec4
ParimalnathReddy <parimalnathreddy@gmail.com>
2025-12-29 15:26:04 -0500

    Changes LCA

--- TREES/Lowest Common Ancestor/LowestCommonAncestor(LCA).py
+++ TREES/Lowest Common Ancestor/LowestCommonAncestor(LCA).py
@@ -1,30 +1,40 @@
 import sys
 
 class Node:
+    # constructor
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
-
+    # string representation
     def __repr__(self):
         return str(self.data)
 
+
+# Function to find the lowest common ancestor (LCA) of two nodes in a binary tree
 def LCA(root, left, right):
+    # Base case: if the tree is empty, return None
     if root is None:
+        # print("Base case: if the tree is empty, return None")
         return None
         
     # Compare data since left/right are new Node instances
     if root.data == left.data or root.data == right.data:
+        # print("Compare data since left/right are new Node instances")
         return root
-    
+    # Recursively find the LCA in the left and right subtrees
     l_lca = LCA(root.left, left, right)
+    # print("l_lca: ", l_lca)
     r_lca = LCA(root.right, left, right)
-    
+    # print("r_lca: ", r_lca)
+    # If both left and right are not None, return the root
     if l_lca and r_lca:
+        # print("l_lca and r_lca: ", l_lca, r_lca)
         return root
-        
+    # If only one of left or right is not None, return that node
     return l_lca if l_lca else r_lca
-        
+
+# Driver code
 if __name__ == "__main__":
     # Representing the following tree
     #         1
